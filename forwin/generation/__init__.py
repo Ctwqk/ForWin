8ef6bdfceaefdb85973e5cdc708530fadf80caf9
ctwqk <taiwei293@gmail.com>
2026-05-20 09:46:01 -0700

    feat: add generation run target semantics

--- forwin/generation/__init__.py
+++ forwin/generation/__init__.py
@@ -1,5 +1,11 @@
 """Generation control helpers."""
 
 from .continue_workset import ContinueGenerationWorkset, build_continue_generation_workset
+from .run_target import GenerationRunTarget, resolve_generation_run_target
 
-__all__ = ["ContinueGenerationWorkset", "build_continue_generation_workset"]
+__all__ = [
+    "ContinueGenerationWorkset",
+    "GenerationRunTarget",
+    "build_continue_generation_workset",
+    "resolve_generation_run_target",
+]
